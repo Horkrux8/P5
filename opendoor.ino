84bcddce42ce9903c3bdbd8c5aea632cfdbcd079
Lennart <lennjb.15@gmail.com>
2022-02-16 16:29:28 +0100

    Host now configures servo degree, added runmode = 3 for configuration, generel code optimization

--- opendoor.ino
+++ opendoor.ino
@@ -1,15 +1,38 @@
+// Library import
+#include "Servo.h"
+
+/* This program is going to wait for a serial message containing
+the identity string, after that it is activating a servoengine */
+
 String incomingString;
-String identityString = "{IDENTIFIER-HOST";
+String HostKey;
+String ClientKey;
+
+// Setup
+Servo door;
 void setup(){
+    door.attach(0);
     Serial.begin(9600);
     Serial.printf("\nCLIENT: Started \n");
+    door.write(0); // Reset servo
 }
+
 void loop(){
+    /* Read incoming serial lines as string and look for identifier
+    if found change servo to found degree*/
+    HostKey = "{IDENTIFIER-HOST}";
+    ClientKey = "{IDENTIFIER-CLIENT}";
+
     if (Serial.available() > 0){
+        // Read all incoming
         incomingString = Serial.readString();
         Serial.print("CLIENT: Found incoming serial: "+incomingString+"\n");
-        if (incomingString.indexOf("{IDENTIFIER-HOST}")){
-            Serial.print("Recognized string");
+
+        if (incomingString.indexOf(HostKey) == 0){
+            // If HostKey in incoming strip HostKey of incoming to find degree
+            Serial.println(ClientKey);
+            int degree = incomingString.substring(HostKey.length()).toInt();
+            door.write(degree); 
         }
     }
 }
--- talking.py
+++ talking.py
@@ -11,7 +11,7 @@ m = sr.Microphone()
 
 language_val = "de-DE"
 magic = "guten tag"
-runmode = 2 # 0 = record / 1 = usepremade audio file / 2 = skip record process
+runmode = 3 # 0 = record / 1 = usepremade audio file / 2 = skip record process / 3 = configure servo
 exitWord = ['quit', 'exit']
 serialcom = {"port":"/dev/ttyUSB0","baud":"9600"}
 
@@ -24,16 +24,17 @@ def usepremade():
     return reply
 
 def record():
+    """Sending a new voice sample with magic word to google for recognition"""
     print("Starting voice recognition")
     print("One moment...")
     with m as source: r.adjust_for_ambient_noise(source) # Take ambient noise into account
     while True:
         print("Now Ready, listening in %s. (Press Ctrl+c to or say exit or quit to quit)" % language_val)
-        with m as source: audio = r.listen(source)
+        with m as source: audio = r.listen(source) # m is microphone (see configuration)
         print("found audio sample")
         try:
             value = r.recognize_google(audio, language=language_val)
-            # decoding google's answer
+            # Decoding google's answer if encoded
             if str is bytes:
                 reply = "{}".format(value).encode("utf-8")
             else:
@@ -43,7 +44,7 @@ def record():
                 quit()
             else:
                 print("You said: %s" % reply)
-        # debugging google errors
+        # Handling google reply errors
         except sr.UnknownValueError:
             print("The Google API could not understand the audio...")
         except sr.RequestError as e:
@@ -52,22 +53,31 @@ def record():
         if reply is not None and not Empty:
             return reply
 
-def openDoor():
+def openDoor(Degree):
     """Send serial message to trigger esp program"""
     print("Sending door opening command over serial")
     port, baud = [value for value in list(serialcom.values())] # Configuring serial
     ser = serial.Serial(port, baud, timeout=1)
     ser.flush()
 
-    ser.write("HOST: {IDENTIFIER-HOST}alalal".encode()) # Send host message
+    HostKey = "{IDENTIFIER-HOST}"
+    ClientKey = "{IDENTIFIER-CLIENT}"
+
+    ser.write((HostKey+str(Degree)).encode()) # Send host message
     while True:
         if ser.in_waiting > 0:
-            data = ser.readline()#.decode('utf-8').rstrip()
-            print(data)
-            #if "{IDENTIFIER-CLIENT}" in data: # search for client message in serial com
-            #    print("recognized client signal")
-            #    ser.close()
-            #    break
+            data = ser.readline().decode('utf-8').rstrip()
+            print("HOST: Found incoming serial: "+data)
+            if ClientKey in data: # search for client message in serial com
+                print("HOST: Recognized client signal")
+                ser.write((HostKey+"0").encode()) if runmode != 3 else None
+                ser.close()
+                break
+
+def configureServo():
+    while True:
+        print("Enter degree")
+        openDoor(input())
 
 def main():
     """Decided runmode and string recognition"""
@@ -78,11 +88,13 @@ def main():
         recstring = usepremade()
     elif runmode == 2:
         recstring = magic
+    elif runmode == 3:
+        configureServo()
 
-    # compare string with the wanted string
+    # Compare string with the wanted string
     if recstring.lower() == magic.lower():
         print("Magic word recognized = %s" % magic)
-        openDoor()
+        openDoor(90)
         #os.system("echo 'your command goes here'")
 
 if __name__ == '__main__':
